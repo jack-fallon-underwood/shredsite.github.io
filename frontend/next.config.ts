72737b397ee046ceb6f4d7ab6ab9ca0304177c31
unknown <jack.underwood@gmail.com>
2025-05-30 19:26:49 -0400

    added output to next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
+// frontend/next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: 'export', // <--- Add this line
+  // Other Next.js configurations go here
 };
 
 export default nextConfig;
